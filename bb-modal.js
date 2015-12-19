4f70cfb230c3f481b0443bcb363d6280c45eb86f
Nihar <niharjyoti333@gmail.com>
2015-12-19 19:31:28 +0530

    Optimize modal

--- bb-modal.js
+++ bb-modal.js
@@ -1,5 +1,5 @@
 /**
- * Author: NIHAR JYOTI SARMA
+ * Author: Nihar Jyoti Sarma
 
  Bootstrap and Backbone common modal
 
@@ -31,28 +31,32 @@
     }
 }(this, function(_, Backbone, $) {
     'use strict';
+
+    // all config attributes
+
+    var defaultOptions = {
+        whereToAppend : 'body',
+        modalId : 'bbModal',
+        modalTitle : '',
+        modalBody : '',
+        modalDisplay :  '.modal',
+        title : false,
+        okText :  'Ok',
+        cancelText : 'Cancel',
+        showFooter :  false,
+        allowCancel :  false
+
+    };
     Backbone.commonModal = {
 
-        modalOpen: function(){
+        modalOpen: function() {
 
-            // all config attributes
-
-            var whereToAppend = (!!this.whereToAppend) ? this.whereToAppend : 'body',
-                modalId = this.modalId || '',
-                modalTitle = this.modalTitle || '',
-                modalBody = this.modalBody || '',
-                modalDisplay = (!!this.modalId) ? '#'+this.modalId : '.modal',
-                title = (!!this.modalTitle) ? true : false,
-                okText = (!!this.okText) ? this.okText : 'Ok',
-                cancelText = (!!this.cancelText) ? this.cancelText : 'Cancel',
-                showFooter = (!!this.showFooter) ? this.showFooter : false,
-                allowCancel = (!!this.allowCancel) ? this.allowCancel : false,
-                cancelText = (!!this.cancelText) ? this.cancelText : false;
+            var opts = $.extend({}, defaultOptions, this);
 
             var template = _.template('\
             <div class="modal fade" id="<%= obj.modalId %>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">\
               <div class="modal-dialog"><div class="modal-content">\
-              <% if (title) { %>\
+              <% if (obj.title) { %>\
                 <div class="modal-header" style="margin-bottom: 0px;  padding: 15px 15px 5px;">\
                   <h3><%= obj.modalTitle %></h3>\
                 </div>\
@@ -61,27 +65,24 @@
               <a href="javascript:void(0)" class="icon icon-circle-sm" data-dismiss="modal" title="close" style="position: absolute;top: -10px;right: -8px;height: 25px;width: 25px;margin: 0;color: #fff;border: 1px solid #DB0A5B;border-radius:30px;background: #DB0A5B;font-size: 17px;font-weight: bold;display: inline-block;line-height: 0px;padding: 12px 6px;">X</a>\
               <% if (obj.showFooter) { %>\
                 <div class="modal-footer">\
-                  <% if (obj.allowCancel) { %>\
-                    <% if (cancelText) { %>\
-                      <a href="#" class="btn cancel close" data-dismiss="modal"><%= cancelText %></a>\
+                    <% if (obj.allowCancel) { %>\
+                      <a href="#" class="btn cancel close" data-dismiss="modal"><%= obj.cancelText %></a>\
                     <% } %>\
-                  <% } %>\
-                  <a href="#" class="btn ok btn-primary"><%= okText %></a>\
+                  <a href="#" class="btn ok btn-primary"><%= obj.okText %></a>\
                 </div>\
               <% } %>\
               </div></div></div>\
-            ')({"obj":this,"cancelText":cancelText,"okText":okText,"title":title});
+            ')({"obj":opts});
 
-            (modalId != '') ? $("#"+modalId).remove(): '';
-            $(whereToAppend).append(template);
-            $(modalDisplay).modal('show');
+            $("#"+opts.modalId).remove();
+            $(opts.whereToAppend).append(template);
+            $("#"+opts.modalId).modal('show');
 
         },
 
-        modalClose: function(){
-            var modalId = this.modalId || '#',
-                modalDisplay = (!!this.modalId) ? '#'+this.modalId : '.modal';
-            $(modalDisplay).modal('hide');
+        modalClose: function() {
+            var opts = $.extend({}, defaultOptions, this);
+            $("#"+opts.modalId).modal('hide');
         }
 
     };
